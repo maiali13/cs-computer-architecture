1ecb7abcf86bb41f64ec864613447f7aaed5b187
maiali13 <maryamfawaz@gmail.com>
2020-07-22 16:41:23 -0700

    step 9

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,7 +6,17 @@ import sys
 HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
+
 MUL = 0b10100010
+DIV = 0b10100011
+ADD = 0b10100000
+SUB = 0b10100001
+
+POP = 0b01000110
+PUSH = 0b01000101
+
+# register num for stack pointer
+SP = 7
 
 class CPU:
     """Main CPU class."""
@@ -18,6 +28,35 @@ class CPU:
         # self.ram = [0] * self.bytes
         self.ram = bytearray(32)
         self.register = bytearray(8)
+        self.register[SP] = 0xF4
+        self.branch_table = {
+            HLT: self.hlt,
+            LDI: self.ldi,
+            PRN: self.prn,
+            POP: self.pop,
+            PUSH: self.push,
+            ADD: self.alu,
+            SUB: self.alu,
+            MUL: self.alu,
+            DIV: self.alu
+        }
+
+    def hlt(self, op_a, op_b):
+        sys.exit()
+
+    def ldi(self, op_a, op_b):
+        self.reg[op_a] = op_b
+
+    def prn(self, op_a, op_b):
+        print(self.reg[op_a])
+    
+    def pop(self, reg_num):
+        self.reg[reg_num] = self.ram_read(self.reg[SP])
+        self.reg[SP] += 1
+
+    def push(self, reg_num):
+        self.reg[SP] -= 1
+        self.ram_write(self.reg[reg_num], self.reg[SP])    
 
     def ram_read(self, address):
         """
@@ -65,16 +104,24 @@ class CPU:
                 self.ram[address] = instruction
                 address += 1
 
-
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
+        # add
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # subtract
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
+        # multiply
         elif op == "MUL":
-            self.reg[reg_a] = self.reg[reg_a] * self.reg[reg_b]
+            self.reg[reg_a] *= self.reg[reg_b]
+        # divide
+        elif op == "DIV":
+            self.reg[reg_a] //= self.reg[reg_b]
+        # modulo
+        elif op == "MOD":
+            self.reg[reg_a] %= self.reg[reg_b]
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -106,20 +153,23 @@ class CPU:
         operand_a = self.ram_read(self.PC + 1)
         operand_b = self.ram_read(self.PC + 2)
 
+        # update for branch table
         while self.IR != HLT: # while cpu is running
-            if self.IR == LDI: # set value of reg to an int
-                self.register[operand_a] = operand_b
+            nums = ((self.IR & 0b11000000) >> 6)
+            try: 
+                if num_args == 0:
+                    self.branch[self.IR]()
 
-            elif self.IR == MUL: # multiplication
-                self.alu("MUL", operand_a, operand_b)
+                elif num_args == 1:
+                    self.branch[self.IR](operand_a)
 
-            elif self.IR == PRN: # print value stored in the reg
-                print(self.register[operand_a])
+                else:
+                    self.branch[self.IR](operand_a, operand_b)
 
-            else:
+            except KeyError:
                 raise Exception('Unsupported operation {self.IR} at address {self.PC}.')
             
-            self.PC += ((self.IR & 0b11000000) >> 6) + 1
+            self.PC += nums + 1
             self.IR = self.ram_read(self.PC)
             operand_a = self.ram_read(self.PC + 1)
             operand_b = self.ram_read(self.PC + 2)
